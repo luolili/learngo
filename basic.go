e444f77e3896be65634e2adeaee7d5365b286c3f
Administrator <2084267015@qq.com>
2019-11-19 21:23:43 +0800

    first go

--- basic.go
+++ basic.go
@@ -1,25 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 func variableZeroValue() {
-
 	var a int
 	var s string
 	fmt.Println(a, s)
 	fmt.Printf("%d %q\n", a, s)
-
 }
 
 func variableInitValue() {
-
 	var a int = 2
 	var s string = "sw"
-
 	fmt.Println(a, s)
 }
+
+func variableTypeDeduction() {
+
+	var a, b, c = 1, "we", 3
+	fmt.Println(a, b, c)
+	//在方法外面不可这样定义var
+	d := 2
+	fmt.Println(d)
+
+}
+
+//包内的var
+var name = "00"
+
 func main() {
 	fmt.Println("halo world")
 	variableZeroValue()
 	variableInitValue()
+	variableTypeDeduction()
+	fmt.Println(name)
+	euler()
+	triangle()
+}
+func euler() {
+	/*c:=3+4i
+	fmt.Println(cmplx.Abs(c))*/
+	c := cmplx.Pow(math.E, 1i*math.Pi) + 1
+	fmt.Println(c)
+
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+
 }
