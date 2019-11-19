822427ccafe23ede339fe706a34055b00a8f9746
Administrator <2084267015@qq.com>
2019-11-19 19:54:38 +0800

    first go

--- basic.go
+++ basic.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func variableZeroValue() {
+
+	var a int
+	var s string
+	fmt.Println(a, s)
+	fmt.Printf("%d %q\n", a, s)
+
+}
+
+func variableInitValue() {
+
+	var a int = 2
+	var s string = "sw"
+
+	fmt.Println(a, s)
+}
 func main() {
 	fmt.Println("halo world")
+	variableZeroValue()
+	variableInitValue()
 }
