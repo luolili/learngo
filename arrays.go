0bd025b7ed56cbb14f0d0434e2dd055493f3f2e3
Administrator <2084267015@qq.com>
2019-11-21 21:47:34 +0800

    first go

---
+++ arrays.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/**
+数组 是 值类型
+*/
+func main() {
+
+	var arr1 [5]int
+	arr2 := [3]int{1, 2, 9}
+	fmt.Println(arr1, arr2)
+	fmt.Println("--", len(arr2))
+
+	for i, v := range arr2 {
+		fmt.Println(i, v)
+	}
+	for _, v := range arr2 {
+		fmt.Println(v)
+	}
+}
