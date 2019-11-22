4ec19d557afc709f0a8ab3dfd746d90121dc0ae7
Administrator <2084267015@qq.com>
2019-11-22 21:48:07 +0800

    first go

--- arrays.go
+++ arrays.go
@@ -9,7 +9,8 @@ func main() {
 
 	var arr1 [5]int
 	arr2 := [3]int{1, 2, 9}
-	fmt.Println(arr1, arr2)
+	arr3 := [...]int{1, 2, 9}
+	fmt.Println(arr1, arr2, arr3)
 	fmt.Println("--", len(arr2))
 
 	for i, v := range arr2 {
