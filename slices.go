fc074dcdc8ae747e175a887f2341287e76ef633b
Administrator <2084267015@qq.com>
2019-11-23 11:58:05 +0800

    first go

--- slices.go
+++ slices.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 /**
-slice 是数组的视图：view
+slice 是数组的视图：view,LOOK
 */
 //参数是slice
 func updateSlice(s []int) {
@@ -23,4 +23,15 @@ func main() {
 	updateSlice(s1)
 	fmt.Println("s1:", s1)   //s1: [100 3 4 5 6 7]
 	fmt.Println("arr:", arr) //s1: [100 3 4 5 6 7]
+
+	fmt.Println("ext:slice")
+	arr2 := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	s3 := arr2[2:6]
+	s4 := s3[3:5]
+	//slice:ptr+len+cap
+	fmt.Println("cap:", cap(s3)) //2-6
+	fmt.Println("len:", len(s3))
+	fmt.Println("s2:", s4)          //可以往后扩展，不可往前扩展
+	fmt.Println("s4 cap:", cap(s4)) //2-6
+	fmt.Println("len:", len(s4))
 }
