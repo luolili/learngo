97cdf6c44b6c3b86a5ea9c46dda4164d4cc3e442
Administrator <2084267015@qq.com>
2019-11-23 20:41:56 +0800

    map

--- retriever/main.go
+++ retriever/main.go
@@ -4,8 +4,38 @@ import "fmt"
 import "./mock"
 import "./real"
 
+//接口
 type Retriever interface {
 	Get(url string) string
+}
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+//接口 的组合
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func post(poster Poster) {
+	poster.Post("https://studygolang.com",
+		map[string]string{
+			"name": "mee",
+			"age":  "11",
+		})
+
+}
+
+const url = "https://studygolang.com"
+
+func session(s RetrieverPoster) string {
+
+	s.Post(url, map[string]string{
+		"contents": "ff",
+	})
+	return s.Get(url)
 }
 
 func download(r Retriever) string {
@@ -17,5 +47,16 @@ func main() {
 	r = mock.Retriever{"this is fake"}
 	fmt.Printf("%T,%v\n", r, r) //类型和值
 	r = real.Retriever{}
+	switch v := r.(type) {
+	case mock.Retriever:
+		fmt.Println("-mock-", v.Contents)
+	case real.Retriever:
+		fmt.Println("-real-", v.UserAgent)
+
+	}
 	//fmt.Println(download(r))
+
+	receiver := mock.Retriever{"this is test"}
+
+	fmt.Println(session(receiver))
 }
--- retriever/mock/mockretriever.go
+++ retriever/mock/mockretriever.go
@@ -7,6 +7,11 @@ type Retriever struct {
 	Contents string
 }
 
+func (r Retriever) Post(url string, form map[string]string) string {
+	r.Contents = form["contents"]
+	return "ok"
+}
+
 //只要实现了这接口
 func (r Retriever) Get(url string) string {
 	return r.Contents
